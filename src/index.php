3bfd42558a67aff468e5fa1a16510bdee2eb90b5
Alex Costa <alex@deanx.com.br>
2016-04-14 07:51:35 -0500

    source code v1

---
+++ src/index.php
@@ -0,0 +1,29 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+require_once(__DIR__ . '/services/lastfm.php');
+$last = new LastFmService();
+
+$artist = $_GET['artist'];
+$country = $_GET['country'];
+
+$page = (int)$_GET['page'] ?: 1;
+
+define(MAX_COUNTRY_ITEMS, 50);
+define(MAX_ARTISTS_PER_PAGE, 5);
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader);
+
+
+if($country) {
+$topArtists = $last->getTopArtistsByCountry($country, $page, MAX_ARTISTS_PER_PAGE);
+echo $twig->render('index.html', array('country' => $country,
+      'artists' => $topArtists, 'num_pages' => MAX_COUNTRY_ITEMS / MAX_ARTISTS_PER_PAGE));
+} elseif($artist) {
+  $artistTracks = $last->getArtistTopTracks($artist);
+  echo $twig->render('artist.html', array('artist' => $artist,
+        'tracks' => $artistTracks));
+}
+else {
+  echo $twig->render('base_template.html');
+}
---
+++ src/services/lastfm.php
@@ -0,0 +1,41 @@
+<?php
+use LastFmApi\Api\AuthApi;
+use LastFmApi\Api\GeoAPI;
+use LastFmApi\Api\ArtistApi;
+
+class LastFmService {
+  public function __construct()
+    {
+        $this->apiKey = '2bd52b75c11bb3a8d3c6c3cebb5a6f8a';
+        $auth = new AuthApi('setsession', array('apiKey' => $this->apiKey));
+        $this->geoAPI = new GeoAPI($auth);
+        $this->artistAPI = new ArtistApi($auth);
+    }
+
+    public function getTopArtistsByCountry($country, $page = 1, $limit = 5)
+    {
+        $topArtists = $this->geoAPI->getTopArtists(array("country" => $country, "page" => $page, "limit" => $limit));
+
+        $artistsParser = function($artist) {
+          return array("name" => $artist["name"], "images" => $artist["image"]);
+        };
+
+        return array_map($artistsParser, $topArtists);
+    }
+
+    private function getArtistInfo($artist) {
+      $artistInfo = $this->artistAPI->getInfo(array("artist" => $artist));
+      return $artistInfo;
+    }
+
+    public function getArtistThumb($artist, $size = "small") {
+      return $this->getArtistInfo($artist)["image"][$size];
+    }
+
+    public function getArtistTopTracks($artist) {
+      $tracks = $this->artistAPI->getTopTracks(array("artist" => $artist));
+            
+      return $tracks;
+    }
+
+}
